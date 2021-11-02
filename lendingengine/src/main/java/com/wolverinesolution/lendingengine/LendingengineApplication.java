0adcbfbc8c39532c8f5baed13a317a95870952bf
Sumit Sarkar <sumit.sarkar@procor.com>
2021-11-01 22:42:54 -0400

    Implement Lombok and Javafaker

--- lendingengine/src/main/java/com/wolverinesolution/lendingengine/LendingengineApplication.java
+++ lendingengine/src/main/java/com/wolverinesolution/lendingengine/LendingengineApplication.java
@@ -1,5 +1,6 @@
 package com.wolverinesolution.lendingengine;
 
+import com.github.javafaker.Faker;
 import com.wolverinesolution.lendingengine.domain.model.Balance;
 import com.wolverinesolution.lendingengine.domain.model.User;
 import com.wolverinesolution.lendingengine.domain.repository.UserRepository;
@@ -7,25 +8,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 @EnableSwagger2
-public class LendingengineApplication implements CommandLineRunner{
-	@Autowired
-	private UserRepository userRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(LendingengineApplication.class, args);
-	}
+public class LendingengineApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(LendingengineApplication.class, args);
+    }
 
-		userRepository.save(new User("Rishabh","Rishabh","Khandelwal",25,"Software Developer",new Balance() ));
-		userRepository.save(new User("Rahul","Rahul","Agrawal",26,"Software Developer", new Balance()));
-		userRepository.save(new User("Gaurav","Gaurav","Patidar",25,"Software Developer", new Balance()));
-		userRepository.save(new User("Munendra","Munendra","Dhayal",23,"Business", new Balance()));
-		userRepository.save(new User("Lekhu","Lekhu","Kumawat",25,"Unemployed", new Balance()));
+    @Override
+    public void run(String... args) throws Exception {
 
-	}
+//        userRepository.save(new User("Rishabh", "Rishabh", "Khandelwal", 25, "Software Developer", new Balance()));
+//        userRepository.save(new User("Rahul", "Rahul", "Agrawal", 26, "Software Developer", new Balance()));
+//        userRepository.save(new User("Gaurav", "Gaurav", "Patidar", 25, "Software Developer", new Balance()));
+//        userRepository.save(new User("Munendra", "Munendra", "Dhayal", 23, "Business", new Balance()));
+//        userRepository.save(new User("Lekhu", "Lekhu", "Kumawat", 25, "Unemployed", new Balance()));
+
+    }
+
+    @Bean
+    CommandLineRunner runner(UserRepository userRepository) {
+        Faker faker = new Faker();
+        return args -> IntStream.range(0, 100).forEach(i -> userRepository.save(new User(
+                faker.name().username(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.number().numberBetween(20, 50),
+                faker.job().position(),
+                new Balance()
+        )));
+    }
 }
